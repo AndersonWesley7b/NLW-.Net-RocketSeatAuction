f0f28e90ff445633966005296f162ba307aa69a0
andersonwesley7b <andersonterra18@gmail.com>
2024-02-06 10:25:54 -0300

    Add project files.

---
+++ src/RocketSeatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketSeatAuction.API.UseCases.Auctions.GetCurrent;
+
+namespace RocketSeatAuction.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCurrentAuction()
+    {
+        var UseCase = new GetCurrentAuctionUseCase();
+
+        var Result = UseCase.Execute();    
+
+        return Ok(Result);
+    }
+    
+
+}
+
---
+++ src/RocketSeatAuction.API/Entities/Auction.cs
@@ -0,0 +1,12 @@
+﻿namespace RocketSeatAuction.API.Entities;
+
+public class Auction
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = String.Empty;
+
+    public DateTime Starts { get; set; }
+    
+    public DateTime Ends { get; set; }
+}
